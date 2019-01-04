173e20de945be535badb1f0892395931cb132e02
Wren Liang <wren.liang@icloud.com>
2019-01-04 09:33:13 -0800

    draft 1

--- Simon Says mk3/ViewController.swift
+++ Simon Says mk3/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redImage: UIImageView!
+    @IBOutlet var blueImage: UIImageView!
+    @IBOutlet var greenImage: UIImageView!
+    @IBOutlet var yellowImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func redButton(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func blueButton(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func greenButton(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func yellowButton(_ sender: UIButton) {
+        
+    }
+    
+    
 
 }
 
---
+++ Simon Says mk3/gameFile.swift
@@ -0,0 +1,14 @@
+//
+//  gameFile.swift
+//  Simon Says mk3
+//
+//  Created by Wren Liang on 2019-01-04.
+//  Copyright © 2019 Wren Liang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class simonGame{
+    
+}
