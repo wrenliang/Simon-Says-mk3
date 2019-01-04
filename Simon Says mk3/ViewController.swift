56475ecdb0d0099c7f24ea8ef310a6bca7fa1ea9
Wren Liang <wren.liang@icloud.com>
2019-01-04 08:56:17 -0800

    Initial Commit

---
+++ Simon Says mk3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simon Says mk3
+//
+//  Created by Wren Liang on 2019-01-04.
+//  Copyright © 2019 Wren Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
